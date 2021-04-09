622266f72329d443ecb9ced26c91a85548cfeed0
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-09 15:57:11 +0200

    first update

---
+++ js/script.js
@@ -0,0 +1,112 @@
+function init() {
+  
+  new Vue({
+
+    el: '#app',
+
+    data: {
+
+      activeChat: '',
+
+      contacts: [
+        {
+          name: 'Michele',
+          avatar: 'male1.png',
+          visible: true,
+          messages: [
+            {
+              date: '10/01/2020 15:30:55',
+              text: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              text: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              text: 'Tutto fatto!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Fabio',
+          avatar: 'male2.jpg',
+          visible: true,
+          messages: [
+              {
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+              },
+              {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+              },
+              {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+              }
+          ],
+        },
+        {
+          name: 'Carla',
+          avatar: 'female1.jpg',
+          visible: true,
+          messages: [
+              {
+                date: '28/03/2020 10:10:40',
+                text: 'La Marianna va in campagna',
+                status: 'received'
+              },
+              {
+                date: '28/03/2020 10:20:10',
+                text: 'Sicura di non aver sbagliato chat?',
+                status: 'sent'
+              },
+              {
+                date: '28/03/2020 16:15:22',
+                text: 'Ah scusa!',
+                status: 'received'
+              }
+          ],
+        },
+        {
+          name: 'Luisa',
+          avatar: 'female2.png',
+          visible: true,
+          messages: [
+              {
+                date: '10/01/2020 15:30:55',
+                text: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+              }
+          ],
+        },
+    ]
+      
+    },
+
+    methods: {
+
+      messageGen: function(index) {
+
+        this.activeChat = this.contacts[index].messages;
+
+      }
+
+    }
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', init);
