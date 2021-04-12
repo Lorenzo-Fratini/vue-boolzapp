494c3b2a230f15df449b0feb32d7234b6647f8fd
LorenzoF <lorenzo.fratini.4@gmail.com>
2021-04-12 15:55:48 +0200

    milestone 4 corrections

--- js/script.js
+++ js/script.js
@@ -7,34 +7,42 @@ function init() {
     data: {
 
       searchUsers: '',
-      allUser: [],
 
-      activeChat: '',
+      activeUser: {},
+
       message: '',
+
       newMessage: {},
+
       defaultAnswere: {
         text: 'Ok',
         status: 'received'
       },
 
+      displayClass: '',
+
       contacts: [
         {
-          name: 'Michele',
+          name: 'Marco',
           avatar: 'male1.png',
+          lastEntry: 'oggi alle 06:50',
           visible: true,
           messages: [
             {
-              date: '10/01/2020 15:30:55',
+              date: '10/01/2020',
+              hour: '15:30',
               text: 'Hai portato a spasso il cane?',
               status: 'sent'
             },
             {
-              date: '10/01/2020 15:50:00',
+              date: '10/01/2020',
+              hour: '15:50',
               text: 'Ricordati di dargli da mangiare',
               status: 'sent'
             },
             {
-              date: '10/01/2020 16:15:22',
+              date: '10/01/2020',
+              hour: '16:15',
               text: 'Tutto fatto!',
               status: 'received'
             }
@@ -43,20 +51,21 @@ function init() {
         {
           name: 'Fabio',
           avatar: 'male2.jpg',
+          lastEntry: 'ieri alle 22:49',
           visible: true,
           messages: [
               {
-                date: '20/03/2020 16:30:00',
+                date: '20/03/2020 16:30',
                 text: 'Ciao come stai?',
                 status: 'sent'
               },
               {
-                date: '20/03/2020 16:30:55',
+                date: '20/03/2020 16:30',
                 text: 'Bene grazie! Stasera ci vediamo?',
                 status: 'received'
               },
               {
-                date: '20/03/2020 16:35:00',
+                date: '20/03/2020 16:35',
                 text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                 status: 'sent'
               }
@@ -65,20 +74,21 @@ function init() {
         {
           name: 'Carla',
           avatar: 'female1.jpg',
+          lastEntry: 'il 10/04/2021 alle 09:12',
           visible: true,
           messages: [
               {
-                date: '28/03/2020 10:10:40',
+                date: '28/03/2020 10:10',
                 text: 'La Marianna va in campagna',
                 status: 'received'
               },
               {
-                date: '28/03/2020 10:20:10',
+                date: '28/03/2020 10:20',
                 text: 'Sicura di non aver sbagliato chat?',
                 status: 'sent'
               },
               {
-                date: '28/03/2020 16:15:22',
+                date: '28/03/2020 16:15',
                 text: 'Ah scusa!',
                 status: 'received'
               }
@@ -87,15 +97,16 @@ function init() {
         {
           name: 'Chiara',
           avatar: 'female2.png',
+          lastEntry: 'oggi alle 08:35',
           visible: true,
           messages: [
               {
-                date: '10/01/2020 15:30:55',
+                date: '10/01/2020 15:30',
                 text: 'Lo sai che ha aperto una nuova pizzeria?',
                 status: 'sent'
               },
               {
-                date: '10/01/2020 15:50:00',
+                date: '10/01/2020 15:50',
                 text: 'Si, ma preferirei andare al cinema',
                 status: 'received'
               }
@@ -109,22 +120,21 @@ function init() {
 
       messageGen: function(index) {
 
-        this.activeChat = this.contacts[index].messages;
-        console.log(this.activeChat);
+        this.activeUser = this.contacts[index];
+
+        this.displayClass = 'display-none';
       },
 
       newMessageGen: function(message) {
 
         this.newMessage = {text: this.message, status: 'sent'};
 
-        this.activeChat.push(this.newMessage);
+        this.activeUser.messages.push(this.newMessage);
         this.message = '';
-
-        
 
         setTimeout(() => {
 
-          this.activeChat.push(this.defaultAnswere);
+          this.activeUser.messages.push(this.defaultAnswere);
           
         }, 1000);
       },
@@ -139,15 +149,17 @@ function init() {
           });
         } else {
 
-          
           this.contacts.forEach(contact => {
 
             const currentContact = contact['name'].toLowerCase();
             const currentUserSearch = this.searchUsers.toLowerCase();
 
-            if (!currentContact.startsWith(currentUserSearch)) {
+            if (!currentContact.includes(currentUserSearch)) {
               
               contact['visible'] = false;
+            } else {
+
+              contact['visible'] = true;
             }
             
           });
